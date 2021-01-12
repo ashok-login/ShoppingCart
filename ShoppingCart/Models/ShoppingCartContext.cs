74696edf75c78c06d056371d3d7a31ac2aec490c
Ashok Kumar Kottagundu <akottagundu@tollplus.com>
2021-01-12 18:10:56 +0530

    Initial commit

---
+++ ShoppingCart/Models/ShoppingCartContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class ShoppingCartContext : IdentityDbContext<IdentityUser>
+    {
+        public ShoppingCartContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
